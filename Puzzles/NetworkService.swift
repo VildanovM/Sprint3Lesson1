78bfbc1fdc2da456c5cd5151a41d04b335cc4511
Maxim Vildanov <vildanov8@icloud.com>
2019-11-22 21:59:40 +0300

    makeCustomQueue

--- Puzzles/NetworkService.swift
+++ Puzzles/NetworkService.swift
@@ -29,54 +29,56 @@ class NetworkService {
 	///  Метод, соединяющий картинки в одну, уже написан (вызывается в конце).
 	///  Ответ передайте в completion.
 	///  Помните, что надо сделать так, чтобы метод работал как можно быстрее.
-	public func loadPuzzle(completion: @escaping (Result<UIImage, Error>) -> ()) {
-		// это адреса картинок. они работающие, всё ок!
-		let firstURL = URL(string: "https://i.imgur.com/JnY1dY7.jpg")!
-		let secondURL = URL(string: "https://i.imgur.com/S93pvex.jpg")!
-		let thirdURL = URL(string: "https://i.imgur.com/pvCHGsL.jpg")!
-		let fourthURL = URL(string: "https://i.imgur.com/DgijrVE.jpg")!
-		let urls = [firstURL, secondURL, thirdURL, fourthURL]
+    public func loadPuzzle(completion: @escaping (Result<UIImage, Error>) -> ()) {
+        // это адреса картинок. они работающие, всё ок!
+        let firstURL = URL(string: "https://i.imgur.com/JnY1dY7.jpg")!
+        let secondURL = URL(string: "https://i.imgur.com/S93pvex.jpg")!
+        let thirdURL = URL(string: "https://i.imgur.com/pvCHGsL.jpg")!
+        let fourthURL = URL(string: "https://i.imgur.com/DgijrVE.jpg")!
+        let urls = [firstURL, secondURL, thirdURL, fourthURL]
         
-		// в этот массив запишите итоговые картинки
-			var results = [UIImage]()
+        // в этот массив запишите итоговые картинки
+        var results = [UIImage]()
+        let myQueue = DispatchQueue(label: "loadPuzzle")
         let groupOfPictures = DispatchGroup()
         for url in urls {
             groupOfPictures.enter()
-            DispatchQueue.global().async {
+            myQueue.async {
                 if let data = try? Data(contentsOf: url) {
                     if let image = UIImage(data: data) {
-                            results.append(image)
+                        results.append(image)
                     }
                 }
                 groupOfPictures.leave()
             }
             
         }
-	
+        
         groupOfPictures.notify(queue: DispatchQueue.main) {
             if let merged = ImagesServices.image(byCombining: results) {
                 completion(.success(merged))
             }
-
-	}
+            
+        }
         
     }
-	
-	
-	// MARK:- Второе задание
-	
-	
-	///  Здесь задание такое:
-	///  У вас есть ключ keyURL, по которому спрятан клад.
-	///  Верните картинку с этим кладом в completion
+    
+    
+    // MARK:- Второе задание
+    
+    
+    ///  Здесь задание такое:
+    ///  У вас есть ключ keyURL, по которому спрятан клад.
+    ///  Верните картинку с этим кладом в completion
     public func loadQuiz(completion: @escaping(Result<UIImage, Error>) -> ()) {
         let keyURL = URL(string: "https://sberschool-c264c.firebaseio.com/enigma.json?avvrdd_token=AIzaSyDqbtGbRFETl2NjHgdxeOGj6UyS3bDiO-Y")!
         let session = URLSession.shared
+        let myQueue = DispatchQueue(label: "loadQuiz")
         let request = URLRequest(url: keyURL, cachePolicy: .reloadIgnoringLocalAndRemoteCacheData)
         let task = session.dataTask(with: request) { (data, response, error) in
             do {
                 let imageLink = try JSONDecoder().decode(String.self, from: data!)
-                DispatchQueue.global().async {
+                myQueue.async {
                     if let data = try? Data(contentsOf: URL(string: imageLink)!) {
                         if let image = UIImage(data: data) {
                             completion(.success(image))
@@ -88,10 +90,10 @@ class NetworkService {
             }
         }
         task.resume()
-		
-		// Вам придёт строка, её надо прочитать с помощью JSONDecoder (ну как мы всегда читали с файрбэйза)
-		
-
+        
+        // Вам придёт строка, её надо прочитать с помощью JSONDecoder (ну как мы всегда читали с файрбэйза)
+        
+        
     }
-
+    
 }
